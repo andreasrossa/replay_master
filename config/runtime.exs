d1ea231720d0663108d541af7abdd31bfd59dfd1
Andreas Rossa <andreas@rossamail.de>
2025-09-26 01:53:11 +0200

    Add app to dev-docker-compos, add env var to enable dashboard

--- config/runtime.exs
+++ config/runtime.exs
@@ -65,6 +65,7 @@ if config_env() == :prod do
     ],
     secret_key_base: secret_key_base
 
+  config :replay_master, dev_routes: System.get_env("ENABLE_DASHBOARD", "false") == "true"
   # ## SSL Support
   #
   # To get SSL working, you will need to add the `https` key
